f862fe84c6527047131cb479675ffc77a952fe6d
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-06 11:06:54 +0100

    add json

--- choices.py
+++ choices.py
@@ -1,3 +1,11 @@
 class Choices:
-    def __init__(self):
-        self.path = []
+    def __init__(self, data):
+        self.data = data
+        self.current_question = None
+
+    def start(self):
+        self.current_question = self.data.get("q")
+    
+    def next_question(self, answer):
+        self.data = self.data.get(answer)
+        self.current_question = self.data.get("q")
--- game.py
+++ game.py
@@ -1,6 +1,33 @@
-from choices import Choices
+import os
+import json
 from player import *
 from choices import *
 
+with open("choices.json", "r") as f: data = json.load(f)
+
+choices = Choices(data)
 player = Player()
-choices = Choices()
+
+game_end = False
+
+def clear():
+    os.system("cls")
+
+def main():
+    choices.start()
+
+    while game_end == False:
+        clear()
+        
+        if(choices.current_question == None):
+            game_end == True
+
+            print("Game End")
+
+            break
+
+        answer = input(choices.current_question + "\n")
+
+        choices.next_question(answer)
+
+main()
